a831f692bac027d86754ab8a81ad1dd0e8dcbb63
platypusrex <frankcooke79@gmail.com>
2018-05-04 09:22:37 -0400

    Rework - remove AuthProvider from client index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from "apollo-boost";
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import { Provider } from 'react-redux';
 import { createReduxStore } from "./redux";
-import { AuthProvider } from "./shared/containers/authState";
 import { apolloClient } from "./api/apiGraphqlUtil";
 import './index.css';
 import { App } from './App';
@@ -14,13 +12,11 @@ const store = createReduxStore();
 
 ReactDOM.render(
 	<ApolloProvider client={apolloClient}>
-		<AuthProvider>
-			<Provider store={store}>
-				<Router>
-					<App />
-				</Router>
-			</Provider>
-		</AuthProvider>
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
 	</ApolloProvider>,
 	document.getElementById('root')
 );
