dddec226e73d3a8cabe0fad8f62fc00df5844a73
platypusrex <frankcooke79@gmail.com>
2018-05-04 09:23:38 -0400

    Add - config-overrides file (needed for adding parsing of .graphql files)

---
+++ client/config-overrides.js
@@ -0,0 +1,6 @@
+const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast');
+
+module.exports = function override(config, env) {
+	config = rewireInlineImportGraphqlAst(config, env);
+	return config;
+};
