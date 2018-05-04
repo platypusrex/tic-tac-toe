97dd0a08e3dac5afd05caf20d55e7d88913b182f
platypusrex <frankcooke79@gmail.com>
2018-05-04 09:20:25 -0400

    Fix - import path for withAllGames HOC in ScoreBoard component

--- client/src/routes/Game/ScoreBoard.js
+++ client/src/routes/Game/ScoreBoard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { compose, withProps } from 'recompose';
 import { Player } from "./Player";
-import { withAllGames } from "../../api/withAllGames";
+import { withAllGames } from "../../api/games/withAllGames";
 import '../../styles/routes/ScoreBoard.css';
 
 export const ScoreBoardComponent = (props) => {
