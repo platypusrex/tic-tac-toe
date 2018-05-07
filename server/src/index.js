a42315c2e8f24df9b020993b859bf059f2e8c451
platypusrex <frankcooke79@gmail.com>
2018-05-06 23:45:33 -0400

    Rework - adds basic setup for web socket connection functionality on server

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
+import { execute, subscribe } from 'graphql';
+import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { typeDefs } from './schema/schema';
 import { resolvers } from './resolvers';
 import { port } from './utils/config';
@@ -17,7 +20,12 @@ const app = express();
 
 app
 	.use(cors('*'))
-	.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}))
+	.use(
+		'/graphiql',
+		graphiqlExpress({
+			endpointURL: '/graphql',
+			subscriptionsEndpoint: `ws://localhost:${port}/subscriptions`
+		}))
 	.use(
 		'/graphql',
 		bodyParser.json(),
@@ -27,7 +35,20 @@ app
 		})
 	);
 
+const ws = createServer(app);
+
 models
 	.sequelize
 	.sync()
-	.then(() => app.listen(port, () => console.log(`listening on port ${port}`)));
+	.then(() => ws.listen(port, () => {
+		console.log(`listening on port ${port}`);
+
+		new SubscriptionServer({
+			execute,
+			subscribe,
+			schema
+		}, {
+			server: ws,
+			path: '/subscriptions'
+		})
+	}));
