b56f789f64b3dd1b0345330d02debd84170b3dcb
platypusrex <frankcooke79@gmail.com>
2018-05-07 22:45:03 -0400

    Rework - adds Subscriptions to graphql resolvers index

--- server/src/resolvers/index.js
+++ server/src/resolvers/index.js
@@ -1,7 +1,12 @@
 import { Query } from './query';
 import { Mutation } from './mutation';
+import { Subscription } from "./subscription";
+import { PubSub } from 'graphql-subscriptions';
+
+export const pubsub = new PubSub();
 
 export const resolvers = {
 	...Query,
-	...Mutation
+	...Mutation,
+	...Subscription
 };
