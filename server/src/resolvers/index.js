f9469b91b128c3cc65403087ccfb3e8f9588412f
platypusrex <frankcooke79@gmail.com>
2018-05-11 15:01:26 -0400

    Rework - imports newly created individual resolvers and export all merged resolvers

--- server/src/resolvers/index.js
+++ server/src/resolvers/index.js
@@ -1,12 +1,13 @@
-import { Query } from './query';
-import { Mutation } from './mutation';
-import { Subscription } from "./subscription";
+import { mergeResolvers } from 'merge-graphql-schemas';
+import { gameResolver } from "./game";
+import { userResolver } from "./user";
+import { commentResolver } from "./comment";
 import { PubSub } from 'graphql-subscriptions';
 
 export const pubsub = new PubSub();
 
-export const resolvers = {
-	...Query,
-	...Mutation,
-	...Subscription
-};
+export const resolvers = mergeResolvers([
+	gameResolver,
+	userResolver,
+	commentResolver
+]);
