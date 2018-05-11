8ca1d194cbc874dab04bc0e827763cc74add1c4b
platypusrex <frankcooke79@gmail.com>
2018-05-11 15:06:39 -0400

    Reformat

--- client/src/api/games/withAllGames.js
+++ client/src/api/games/withAllGames.js
@@ -1,6 +1,10 @@
 import { graphql } from 'react-apollo';
 import AllGamesQuery from './allGamesQuery.graphql';
 
+// unnecessary comment again
+
 export const withAllGames = graphql(AllGamesQuery, {
-	options: () =>  ({fetchPolicy: 'cache-and-network'})
+	options: () =>  ({
+		fetchPolicy: 'cache-and-network'
+	})
 });
