0a9de11a675442f0c8e4ecb00574d9f35faafd8a
platypusrex <frankcooke79@gmail.com>
2018-05-11 15:05:45 -0400

    Rework - updates sequelize config to underscore all fields, adds UserGame model to exported db, adds db logic for any associations between models

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,20 +3,24 @@ import { admin, database, password } from '../utils/config';
 
 const sequelize = new Sequelize(database, admin, password, {
 	host: 'localhost',
-	dialect: 'postgres'
+	dialect: 'postgres',
+	define: {
+		underscored: true
+	}
 });
 
 const db = {
 	User: sequelize.import('./user'),
 	Game: sequelize.import('./game'),
+	UserGame: sequelize.import('./userGame'),
 	Comment: sequelize.import('./comment')
 };
 
-// Object.keys(db).forEach(modelName => {
-// 	if (db[modelName].associate) {
-// 		db[modelName].associate(db);
-// 	}
-// });
+Object.keys(db).forEach(modelName => {
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
+});
 
 db.sequelize = sequelize;
 
